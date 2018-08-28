ad9845b4b1c0d5c580b9f75d14b2c73576e5ee7e
JEMD <x_jemd_x@hotmail.com>
2018-08-28 11:12:25 -0300

    faltan los estados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProvidersApiServiceProvider } from '../../providers/providers-api-service/providers-api-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,260 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  lights = {
+    cowork: false,
+    workshopCentral: false,
+    workshopLateral: false,
+    impresoras3D: false,
+    ideas1: false,
+    ideas2: false,
+    freeWorkDerecho: false,
+    freeWorkIzquierdo: false,
+    innoBar: false,
+    letrero: false
+  }
 
+  constructor(public navCtrl: NavController, private api: ProvidersApiServiceProvider) { }
+
+  ionViewDidLoad() {
+    console.log('panel de switches')
+    console.log(this.state('cowork'))
+    this.state('cowork')
+    this.state('workshop cental')
+  }
+
+  action(place:string){
+    console.log(`action`)
+    switch(place){
+      case 'cowork':
+      console.log(this.lights.cowork)
+      if(this.lights.cowork == true){
+        this.lights.cowork = false
+        this.turnOff(place)
+      }else{
+        this.lights.cowork = true
+        this.turnOn(place)
+      }
+      break
+      case 'workshop cental':
+      console.log(this.lights.workshopCentral)
+      if(this.lights.workshopCentral == true){
+        this.lights.workshopCentral = false
+        this.turnOff(place)
+      }else{
+        this.lights.workshopCentral = true
+        this.turnOn(place)
+      }
+      break
+      case 'workshop lateral':
+      console.log(this.lights.workshopLateral)
+      if(this.lights.workshopLateral == true){
+        this.lights.workshopLateral = false
+        this.turnOff(place)
+      }else{
+        this.lights.workshopLateral = true
+        this.turnOn(place)
+      }
+      break
+      case 'impresoras 3d':
+      console.log(this.lights.impresoras3D)
+      if(this.lights.impresoras3D == true){
+        this.lights.impresoras3D = false
+        this.turnOff(place)
+      }else{
+        this.lights.impresoras3D = true
+        this.turnOn(place)
+      }
+      break
+      case 'ideas 1':
+      console.log(this.lights.ideas1)
+      if(this.lights.ideas1 == true){
+        this.lights.ideas1 = false
+        this.turnOff(place)
+      }else{
+        this.lights.ideas1 = true
+        this.turnOn(place)
+      }
+      break
+      case 'ideas 2':
+      console.log(this.lights.ideas2)
+      if(this.lights.ideas2 == true){
+        this.lights.ideas2 = false
+        this.turnOff(place)
+      }else{
+        this.lights.ideas2 = true
+        this.turnOn(place)
+      }
+      break
+      case 'free work derecho':
+      console.log(this.lights.freeWorkDerecho)
+      if(this.lights.freeWorkDerecho == true){
+        this.lights.freeWorkDerecho = false
+        this.turnOff(place)
+      }else{
+        this.lights.freeWorkDerecho = true
+        this.turnOn(place)
+      }
+      break
+      case 'free work izquierdo':
+      console.log(this.lights.freeWorkIzquierdo)
+      if(this.lights.freeWorkIzquierdo == true){
+        this.lights.freeWorkIzquierdo = false
+        this.turnOff(place)
+      }else{
+        this.lights.freeWorkIzquierdo = true
+        this.turnOn(place)
+      }
+      break
+      case 'inno-bar':
+      console.log(this.lights.innoBar)
+      if(this.lights.innoBar == true){
+        this.lights.innoBar = false
+        this.turnOff(place)
+      }else{
+        this.lights.innoBar = true
+        this.turnOn(place)
+      }
+      break
+      case 'letrero':
+      console.log(this.lights.letrero)
+      if(this.lights.letrero == true){
+        this.lights.letrero = false
+        this.turnOff(place)
+      }else{
+        this.lights.letrero = true
+        this.turnOn(place)
+      }
+      break
+    }
+  }
+
+  turnOn(place:string){
+    console.log(`turn on`)
+    switch(place){
+      case 'cowork':
+      this.lights.cowork = true
+      break
+      case 'workshop cental':
+      this.lights.workshopCentral = true
+      break
+      case 'workshop lateral':
+      this.lights.workshopLateral = true
+      break
+      case 'impresoras 3d':
+      this.lights.impresoras3D = true
+      break
+      case 'ideas 1':
+      this.lights.ideas1 = true
+      break
+      case 'ideas 2':
+      this.lights.ideas2 = true
+      break
+      case 'free work derecho':
+      this.lights.freeWorkDerecho = true
+      break
+      case 'free work izquierdo':
+      this.lights.freeWorkIzquierdo = true
+      break
+      case 'inno-bar':
+      this.lights.innoBar = true
+      break
+      case 'letrero':
+      this.lights.letrero = true
+      break
+    }
+    this.api.turnOnLight(place)
+    .then((res:any) =>{
+      this.state(place)
+    })
+    .catch((reason) => {
+      console.log('Error', reason)
+    })
+  }
+
+  turnOff(place:string){
+    console.log(`turn off`)
+    switch(place){
+      case 'cowork':
+      this.lights.cowork = false
+      break
+      case 'workshop cental':
+      this.lights.workshopCentral = false
+      break
+      case 'workshop lateral':
+      this.lights.workshopLateral = false
+      break
+      case 'impresoras 3d':
+      this.lights.impresoras3D = false
+      break
+      case 'ideas 1':
+      this.lights.ideas1 = false
+      break
+      case 'ideas 2':
+      this.lights.ideas2 = false
+      break
+      case 'free work derecho':
+      this.lights.freeWorkDerecho = false
+      break
+      case 'free work izquierdo':
+      this.lights.freeWorkIzquierdo = false
+      break
+      case 'inno-bar':
+      this.lights.innoBar = false
+      break
+      case 'letrero':
+      this.lights.letrero = false
+      break
+    }
+    this.api.turnOffLight(place)
+    .then((res:any) =>{
+      this.state(place)
+    })
+    .catch((reason) => {
+      console.log('Error', reason)
+    })
+  }
+
+  state(place:string){
+    console.log(`state`)
+    this.api.statusLight(place)
+    .then((res:any) =>{
+      switch(place){
+        case 'cowork':
+        this.lights.cowork = (place = '1') ? true:false
+        break
+        case 'workshop cental':
+        this.lights.workshopCentral = (place = '1') ? true:false
+        break
+        case 'workshop lateral':
+        this.lights.workshopLateral = (place = '1') ? true:false
+        break
+        case 'impresoras 3d':
+        this.lights.impresoras3D = (place = '1') ? true:false
+        break
+        case 'ideas 1':
+        this.lights.ideas1 = (place = '1') ? true:false
+        break
+        case 'ideas 2':
+        this.lights.ideas2 = (place = '1') ? true:false
+        break
+        case 'free work derecho':
+        this.lights.freeWorkDerecho = (place = '1') ? true:false
+        break
+        case 'free work izquierdo':
+        this.lights.freeWorkIzquierdo = (place = '1') ? true:false
+        break
+        case 'inno-bar':
+        this.lights.innoBar = (place = '1') ? true:false
+        break
+        case 'letrero':
+        this.lights.letrero = (place = '1') ? true:false
+        break
+      }
+    })
+    .catch((reason) => {
+      console.log('Error', reason)
+    })
   }
 
 }
---
+++ src/providers/providers-api-service/providers-api-service.ts
@@ -0,0 +1,168 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ProvidersApiServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ProvidersApiServiceProvider {
+
+  constructor(public api: HttpClient) { }
+
+  turnOnLight(place:string){
+    let data = {
+      ip: null,
+      port: 81,
+      id: null
+    }
+    switch(place){
+      case 'cowork':
+        data.ip = '192.168.0.80'
+        data.id = 4
+      break
+      case 'workshop cental':
+        data.ip = '192.168.0.51'
+        data.id = 1
+      break
+      case 'workshop lateral':
+        data.ip = '192.168.0.58'
+        data.id = 8
+      break
+      case 'impresoras 3d':
+        data.ip = '192.168.0.61'
+        data.id = 10
+      break
+      case 'ideas 1':
+        data.ip = '192.168.0.57'
+        data.id = 7
+      break
+      case 'ideas 2':
+        data.ip = '192.168.0.56'
+        data.id = 6
+      break
+      case 'free work derecho':
+        data.ip = '192.168.0.55'
+        data.id = 5
+      break
+      case 'free work izquierdo':
+        data.ip = '192.168.0.52'
+        data.id = 2
+      break
+      case 'inno-bar':
+        data.ip = '192.168.0.59'
+        data.id = 9
+      break
+      case 'letrero':
+        data.ip = '192.168.0.62'
+        data.id = 12
+      break
+    }
+    return this.api.get(`http://${data.ip}:${data.port}/onled/${data.id}`).toPromise()
+  }
+
+  turnOffLight(place:string){
+    let data = {
+      ip: null,
+      port: 81,
+      id: null
+    }
+    switch(place){
+      case 'cowork':
+        data.ip = '192.168.0.80'
+        data.id = 4
+      break
+      case 'workshop cental':
+        data.ip = '192.168.0.51'
+        data.id = 1
+      break
+      case 'workshop lateral':
+        data.ip = '192.168.0.58'
+        data.id = 8
+      break
+      case 'impresoras 3d':
+        data.ip = '192.168.0.61'
+        data.id = 10
+      break
+      case 'ideas 1':
+        data.ip = '192.168.0.57'
+        data.id = 7
+      break
+      case 'ideas 2':
+        data.ip = '192.168.0.56'
+        data.id = 6
+      break
+      case 'free work derecho':
+        data.ip = '192.168.0.55'
+        data.id = 5
+      break
+      case 'free work izquierdo':
+        data.ip = '192.168.0.52'
+        data.id = 2
+      break
+      case 'inno-bar':
+        data.ip = '192.168.0.59'
+        data.id = 9
+      break
+      case 'letrero':
+        data.ip = '192.168.0.62'
+        data.id = 12
+      break
+    }
+    return this.api.get(`http://${data.ip}:${data.port}/offled/${data.id}`).toPromise()
+  }
+
+  statusLight(place:string){
+    let data = {
+      ip: null,
+      port: 81,
+      id: null
+    }
+    switch(place){
+      case 'cowork':
+        data.ip = '192.168.0.80'
+        data.id = 4
+      break
+      case 'workshop cental':
+        data.ip = '192.168.0.51'
+        data.id = 1
+      break
+      case 'workshop lateral':
+        data.ip = '192.168.0.58'
+        data.id = 8
+      break
+      case 'impresoras 3d':
+        data.ip = '192.168.0.61'
+        data.id = 10
+      break
+      case 'ideas 1':
+        data.ip = '192.168.0.57'
+        data.id = 7
+      break
+      case 'ideas 2':
+        data.ip = '192.168.0.56'
+        data.id = 6
+      break
+      case 'free work derecho':
+        data.ip = '192.168.0.55'
+        data.id = 5
+      break
+      case 'free work izquierdo':
+        data.ip = '192.168.0.52'
+        data.id = 2
+      break
+      case 'inno-bar':
+        data.ip = '192.168.0.59'
+        data.id = 9
+      break
+      case 'letrero':
+        data.ip = '192.168.0.62'
+        data.id = 12
+      break
+    }
+    return this.api.get(`http://${data.ip}:${data.port}/estado/${data.id}`).toPromise()
+  }
+
+}
